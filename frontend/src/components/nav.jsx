de4cde2696c8ae72ab26fc90e04afd0a7429fbff
Biraj Buddhacharya <otaku000god@gmail.com>
2025-01-26 19:33:28 +0545

    Frontend: Small responsive adjustments

--- frontend/src/components/nav.jsx
+++ frontend/src/components/nav.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Nav() {
   return (
     <header className="w-full flex justify-center">
-      <nav className="flex gap-12 whitespace-nowrap bg-primary px-16 py-4 rounded-full">
+      <nav className="flex gap-12 whitespace-nowrap from-primary to-[#0C1839] via-primary px-16 py-4 rounded-full bg-linear-90">
         <Link className="text-white" to="/">Home</Link>
         <Link className="text-white" to="/InternshipHub">Internship Hub</Link>
         <Link className="text-white" to="/ResumeBuilder">Resume Builder</Link>
--- frontend/src/sections/hero.jsx
+++ frontend/src/sections/hero.jsx
@@ -1,28 +1,29 @@
-import globe from "../assets/img/globe.svg";
+import globe from "../assets/img/globe.png";
 import { Link } from "react-router-dom";
 import rightArrow from "../assets/icons/rightArrow.svg";
 
 export default function Hero() {
   return (
     <>
-      <div className="h-full w-full flex flex-col justify-center items-center overflow-hidden">
-        <div className="flex flex-col items-center mt-32">
-          <h1 className="text-4xl font-bold drop-shadow-[0_20px_20xp_rgba(255,255,255,1)]">Internship Hub</h1>
+      <div className="h-full w-full flex flex-col justify-between items-center gap-4">
+        <div className="flex flex-col items-center justify-end h-[65%] relative z-10">
+          <h1 className="text-6xl font-bold">Internship Hub</h1>
           <p className="text-lg">Your next internship is just a click away</p>
           <Link to="/InternshipHub">
-            <div className="py-3 px-7 bg-primary rounded-full relative z-10 flex justify-center items-center gap-2">
+            <div className="mt-2 py-3 px-7 bg-primary rounded-full relative z-10 flex justify-center items-center gap-2">
               <span>Explore internship</span>
               <img src={rightArrow} alt="" />
             </div>
           </Link>
         </div>
 
-        <div className="relative w-full h-full">
-          <div className="h-[64rem] w-[64rem] bg-primary absolute rounded-full left-1/2 transform -translate-x-1/2 blur-[3rem] opacity-90"></div>
+        <div style={{position: "relative"}} className="w-full h-full grow">
+          <div className="h-[90rem] w-[90rem] bg-primary absolute rounded-full left-1/2 transform -translate-x-1/2 blur-[100rem] opacity-90"></div>
           <img
             src={globe}
+            style={{}}
             alt="earth globe image"
-            className="h-[64rem] absolute left-1/2 transform -translate-x-1/2 top-[0%]"
+            className="h-[200%] absolute left-1/2 transform -translate-x-1/2"
           />
         </div>
       </div>
